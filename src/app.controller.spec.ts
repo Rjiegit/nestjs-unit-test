43309250d115b3213f39e3fa919232ea2326ead0
Jie <mjunzh@gmail.com>
2019-05-31 23:59:36 +0800

    add a example

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,21 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { createStubInstance } from 'sinon';
 
 describe('AppController', () => {
   let app: TestingModule;
+  let appController: AppController;
+  let appService: AppService;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: AppService,
+          useValue: createStubInstance(AppService),
+        },
+      ],
     }).compile();
+
+    appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
   describe('getHello', () => {
     it('should return "Hello World!"', () => {
-      const appController = app.get<AppController>(AppController);
+      jest.spyOn(appService, 'getHello').mockImplementationOnce(() => 'Hello World!');
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('getData', () => {
+    it('should call getData twice', async () => {
+      jest
+        .spyOn(appService, 'getData')
+        .mockImplementationOnce(async () => 1)
+        .mockImplementationOnce(async () => 2);
+
+      expect(await appController.getData()).toEqual([1, 2]);
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,18 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('data')
+  async getData() {
+    let count = 0;
+    const collection = [];
+
+    do {
+      const result = await this.appService.getData();
+      collection.push(result);
+      count++;
+    } while (count < 2);
+
+    return collection;
+  }
 }
